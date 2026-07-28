f319e7f399257a3bd343cb62b5ad90e23f7aa171
Tyler Waldo <tylerscott2782@gmail.com>
2026-07-27 19:09:12 -0500

    add file upload functionality to Home component

--- src/jsx/Home.jsx
+++ src/jsx/Home.jsx
@@ -1,6 +1,42 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+function FileUploadTest() {
+    const [fileUploadMessage, setFileUploadMessage] = useState('')
+    const [imgSrc, setImgSrc] = useState('http://localhost:5093/storedFile/download/babe6c36-60e0-40f3-9d96-c60bac9cc2e7');
+
+    async function onChangeFileInput(e) {
+        const file = e.target.files[0];
+        if (!file) return
+
+        const formData = new FormData();
+        formData.append('file', file)
+
+        setFileUploadMessage('Uploading...')
+        const response = await fetch(`${API_BASE_URL}/storedFile`, {
+            method: 'POST',
+            credentials: "include",
+            body: formData,
+        })
+
+        if (!response.ok) {
+            setFileUploadMessage('Something went wrong')
+        }
+
+        setFileUploadMessage('Uploaded')
+
+        const data = await response.json()
+        setImgSrc(`${API_BASE_URL}/storedFile/download/${data.id}`)
+    }
+
+    return <>
+        <input type="file" onChange={onChangeFileInput} />
+        <div>{fileUploadMessage}</div>
+        <img src={imgSrc} />
+    </>
+}
 
 export default function Home() {
     const navigate = useNavigate()
@@ -18,5 +54,7 @@ export default function Home() {
 
     return <>
         <button onClick={handleClickLogoutButton}>Logout</button>
+        <br />
+        <FileUploadTest />
     </>
 }
