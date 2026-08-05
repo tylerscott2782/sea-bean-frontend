5e87583afb407312cfb28311f849ee062f86ecdc
Tyler Waldo <tylerscott2782@gmail.com>
2026-08-04 21:57:50 -0500

    correct modal sizing for mobile

--- src/jsx/Modal.jsx
+++ src/jsx/Modal.jsx
@@ -4,8 +4,8 @@ export function Modal({ isOpen, setIsOpen, children }) {
     if (!isOpen) return null
 
     return createPortal(
-        <div style={{ position: "fixed", top: "0", width: "100dvw", height: "100dvh", backgroundColor: "#00000099" }}>
-            <div style={{ maxWidth: "1000px", width: "100%", height: "800px", maxHeight: "95%", backgroundColor: "#1c202c", borderRadius: "30px", margin: "5% auto", padding: "30px" }}>
+        <div style={{ position: "fixed", top: "0", width: "100dvw", height: "100dvh", backgroundColor: "#00000099", paddingBottom: "10%" }}>
+            <div style={{ maxWidth: "1000px", maxHeight: "100%", height: "800px", backgroundColor: "#1c202c", borderRadius: "30px", margin: "5% auto 0 auto", padding: "30px 30px 60px" }}>
                 <div style={{ display: "flex", justifyContent: "space-between", height: "45px", maxHeight: "45px" }}>
                     <div style={{ fontSize: "20px", marginBottom: "20px" }}><strong>New Sea Bean Entry</strong></div>
                     <div
